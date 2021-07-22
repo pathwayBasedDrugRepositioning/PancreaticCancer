a3a624ad8e550102b4b652b7fdb1db0d6fe6abe9
pathwayBased DrugRepositioning <39158335+pathwayBasedDrugRepositioning@users.noreply.github.com>
2021-07-22 17:42:00 +0100

    Add files via upload

--- SynergyFigs.R
+++ SynergyFigs.R
@@ -1,7 +1,7 @@
   library("ggplot2")
   library("cowplot")
   library("magick")
-
+  library(stringr)
 figs4drug<-function(drugName,cell="PANC-1"){
   
   
@@ -33,6 +33,7 @@ figs4drug<-function(drugName,cell="PANC-1"){
   
   dose=ggdraw() +  draw_image(dose);
   bar=ggdraw() +  draw_image(bar);
+  drugName=str_split(drugName,"_")[[1]][1]
   label=paste0(drugName, " vs Gemcitabine on ",cell) 
   ########## all figs
   title <- ggdraw() + draw_label(label)
@@ -41,6 +42,8 @@ figs4drug<-function(drugName,cell="PANC-1"){
   pp3=plot_grid(title,pp2,bar,nrow=3,rel_heights=c(0.1,1,1))
   ###
   ggsave(filename = paste0("./figs/supplementaryFig_",label,".tiff"),plot = pp3,width = 21, height = 28, dpi = 600,units="cm", compression = "lzw")
+  
+  ggsave(filename = paste0("./figs/supplementaryFig_",label,".pdf"),plot = pp3,width = 21, height = 28, dpi = 600,units="cm")
   return(pp)
 }
 gen_fig3<-function(){
@@ -55,11 +58,22 @@ gen_fig3<-function(){
   f9=figs4drug("BX795")
   f10=figs4drug("Semagacestat")
   
+  f11=figs4drug("Entinostat_HPAF-II",cell="HPAF-II")
+  f12=figs4drug("Entinostat_K8484",cell="K8484")
+  f13=figs4drug("Entinostat_MIA PaCa-2",cell="MIA PaCa-2")
+  f14=figs4drug("Entinostat_TB32048",cell="TB32048")
+  
   pp=plot_grid(f1,f2,f3,f4,f5,ncol=1,labels=LETTERS[1:5])
   pp2=plot_grid(f6,f7,f8,f9,f10,ncol=1,labels=LETTERS[1:5])
+  pp3=plot_grid(f11,f12,f13,f14,f1,ncol=1,labels=LETTERS[1:5])
 
   ggsave(filename = paste0("./figs/fig3.tiff"),plot = pp,width = 11, height = 8.3, dpi = 600,units="in", compression = "lzw")
   ggsave(filename = paste0("./figs/fig4.tiff"),plot = pp2,width = 11, height = 8.3, dpi = 600,units="in", compression = "lzw")
+  ggsave(filename = paste0("./figs/SupplementaryFig_Entinostat.tiff"),plot = pp3,width = 11, height = 8.3, dpi = 600,units="in", compression = "lzw")
+  
+  ggsave(filename = paste0("./figs/fig3.pdf"),plot = pp,width = 11, height = 8.3, dpi = 600,units="in")
+  ggsave(filename = paste0("./figs/fig4.pdf"),plot = pp2,width = 11, height = 8.3, dpi = 600,units="in")
+  ggsave(filename = paste0("./figs/SupplementaryFig_Entinostat.pdf"),plot = pp3,width = 11, height = 8.3, dpi = 600,units="in")
 
 }
 gen_fig5<-function(){
@@ -76,14 +90,19 @@ gen_fig5<-function(){
   fig_zip=ggdraw() +  draw_image(zip);
   
   pp=plot_grid(fig_bliss,fig_loewe,ncol=1,labels=LETTERS[1:2])
-pp2=plot_grid(fig_HSA,fig_zip,ncol=1,labels=LETTERS[1:2])
+  pp2=plot_grid(fig_HSA,fig_zip,ncol=1,labels=LETTERS[1:2])
  
- ggsave(filename = paste0("./figs/fig5.tiff"),plot = pp,width = 8, height = 11, dpi = 600,units="in", compression = "lzw")
- ggsave(filename = paste0("./figs/SupplementaryFig_synergy.tiff"),plot = pp2,width = 8, height = 11, dpi = 600,units="in", compression = "lzw")
- 
+  ggsave(filename = paste0("./figs/fig5.tiff"),plot = pp,width = 8, height = 11, dpi = 600,units="in", compression = "lzw")
+  ggsave(filename = paste0("./figs/SupplementaryFig_synergy.tiff"),plot = pp2,width = 8, height = 11, dpi = 600,units="in", compression = "lzw")
+  
+  ggsave(filename = paste0("./figs/fig5.pdf"),plot = pp,width = 8, height = 11, dpi = 600,units="in")
+  ggsave(filename = paste0("./figs/SupplementaryFig_synergy.pdf"),plot = pp2,width = 8, height = 11, dpi = 600,units="in")
+
+  
   xx=1
 }
 
 #figs4drug("Entinostat")
 gen_fig3()
 gen_fig5()
+print("Happy")
